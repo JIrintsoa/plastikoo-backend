88751da12bb4888faac6f388db720c9eba1ff06a
JIrintsoa <johns.irintsoa@gmail.com>
2024-09-12 14:55:51 +0300

    add update into the config database

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,19 @@
-import mysql from "mysql2";
-import "dotenv/config";
+import mysql from 'mysql2'
+import 'dotenv/config'
 
 export const mysqlPool = mysql.createPool({
-  host: process.env.DB_DEV_HOST,
-  user: process.env.DB_DEV_USER,
-  password: process.env.DB_DEV_PASSWORD,
-  database: process.env.DB_DEV_NAME,
+    host: process.env.DB_DEV_HOST,
+    user: process.env.DB_DEV_USER,
+    password: process.env.DB_DEV_PASSWORD,
+    database: process.env.DB_DEV_NAME,
 });
 
-export default mysqlPool;
+export const mysqlPoolMachine = mysql.createPool({
+    host: process.env.DB_DEV_HOST,
+    user: process.env.DB_DEV_USER_MACHINE,
+    password: process.env.DB_DEV_USER_MACHINE_MDP,
+    database: process.env.DB_DEV_NAME
+})
+
+export default  mysqlPool;
+

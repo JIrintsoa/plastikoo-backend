5569a0a2bf9de151be89194e5e3e5409b1398117
JIrintsoa <johns.irintsoa@gmail.com>
2024-09-18 17:23:50 +0300

    update utilisation de ticket
    nasiana fonctionnalite:
    mi-ajouter an'ilay recolte,
    dia authentifier,
    utiliser le ticket

--- src/routes/ticket.js
+++ src/routes/ticket.js
@@ -1,11 +1,16 @@
 import { Router } from "express";
 import TicketController from "../controllers/ticket.js";
+import TransactionController from "../controllers/transaction.gains.js";
+import AuthenticationController from "../utils/authentication.js";
 
 const route = Router()
 
 route.get('/:id_ticket',
+    AuthenticationController.verifyRoleToken('utilisateur'),
     TicketController.infos,
-    TicketController.afficher
+    TicketController.afficher,
+    TransactionController.recolte,
+    TicketController.utilise
 )
 
 // route.get('/')

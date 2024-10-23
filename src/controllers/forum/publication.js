287af38db7776c7e975a5fab4e2cd6df342d96d6
Johns from EC2 <johns.irintsoa@gmail.com>
2024-10-23 11:29:31 +0300

    update the publication mysql import

--- src/controllers/forum/publication.js
+++ src/controllers/forum/publication.js
@@ -1,5 +1,5 @@
 import { z,ZodError } from "zod";
-import mysqlPool from "../../config/database.js";
+import {mysqlPool} from "../../config/database.js";
 import UploadController from "../upload.js";
 
 import 'dotenv/config'
